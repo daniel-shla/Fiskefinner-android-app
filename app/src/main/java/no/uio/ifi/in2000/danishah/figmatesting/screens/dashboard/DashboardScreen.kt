5808210b7bf6763bc5a975aab70eab44041e4789
danishah <danishah@uio.no>
2025-03-27 10:29:37 +0100

    wtf

--- app/src/main/java/no/uio/ifi/in2000/danishah/figmatesting/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/danishah/figmatesting/screens/dashboard/DashboardScreen.kt
@@ -1,13 +1,20 @@
 package no.uio.ifi.in2000.danishah.figmatesting.screens.dashboard
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
